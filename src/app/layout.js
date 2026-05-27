85cefbbaa199ebadfa95c99dedab77d3282d0f2b
Raisul <raisulconnects@gmail.com>
2026-05-27 17:01:57 +0600

    Proxy.js working fine now

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,3 @@
-import dns from "node:dns";
-dns.setServers(["8.8.8.8", "8.8.8.4"]);
-
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.8.4"]);
 
 const client = new MongoClient(process.env.MONGO_DB_URI);
 const db = client.db();
